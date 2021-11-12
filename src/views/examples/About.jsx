14dee3cfa50b444df68b99346a44de83313fc9e5
EversonBorges <everson.cursos@gmail.com>
2021-11-12 08:27:39 -0300

    Primeiro Commit

---
+++ src/views/examples/About.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const About = props => (
+    <aside className="About">
+        <h1> Sobre </h1>
+        <h2>Nosso Sistema foi criado...</h2>
+    </aside>
+)
+export default About;
